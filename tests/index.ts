f8e78a8a124cac89a46c511bbbb6eaa920fc899a
Val-istar-Guo <val.istar.guo@gmail.com>
2019-07-19 15:09:20 +0800

    test: add test

--- tests/index.ts
+++ tests/index.ts
@@ -1,3 +1,23 @@
 import test from 'ava'
+import ejs from '@/index'
 
-test.todo('write your test here...')
+test('ejs handler', async t => {
+  const handler = ejs()
+
+  t.is(await handler.genPath('/a/b/c.ejs'), '/a/b/c')
+  const file = {
+    content: `<%= project.name %>`,
+    addition: {},
+  }
+
+  const resource = {
+    mili: { version: '0.0.0' },
+    project: { name: 'test' },
+    template: {},
+    answers: {},
+  }
+
+  await handler.genFile(file, resource)
+
+  t.snapshot(file.content)
+})
